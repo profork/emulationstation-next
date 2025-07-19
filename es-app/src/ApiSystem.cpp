02ffae2f220a547c8f3e11ee0472b83ea2c2fd9c
spycat88 <spycat88@users.noreply.github.com>
2025-07-19 20:20:57 +0100

    Fixup following last merge, remove bezel 16:9 from scraper settings again and revert suspend location

--- es-app/src/ApiSystem.cpp
+++ es-app/src/ApiSystem.cpp
@@ -2282,7 +2282,7 @@ bool ApiSystem::emuKill()
 void ApiSystem::suspend()
 {
 	LOG(LogDebug) << "ApiSystem::suspend";
-	executeScript("/usr/bin/batocera-shutdown 1");
+	executeScript("/usr/sbin/pm-suspend");
 }
 
 void ApiSystem::replugControllers_sindenguns()
